4dae0f83f2b2a0f59cb98033a75b14d3c37173b2
Yurcell <chestayurcel@gmail.com>
2026-01-02 13:21:06 +0700

    Menambahkan middleware untuk melayani file statis dari direktori 'uploads'

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 const { db } = require('./src/models');
 const roomRoutes = require('./src/routes/roomRoutes');
 const authRoutes = require('./src/routes/authRoutes');
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api/rooms', roomRoutes);
 app.use('/api/auth', authRoutes);
