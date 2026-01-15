3dd4bf34d007923db830dbb37cc2861c6dc18c96
Yurcell <chestayurcel@gmail.com>
2026-01-15 16:13:34 +0700

    menambahkan prefix api

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,7 +1,7 @@
 // frontend/src/config/api.js
 
 // GANTI string di bawah ini dengan URL Backend Vercel kamu yang asli
-const SERVER_URL = 'https://permatakost-api.vercel.app/'; 
+const SERVER_URL = 'https://permatakost-api.vercel.app/api';    
 
 const API_BASE_URL = import.meta.env.MODE === 'production'
   ? `${SERVER_URL}/api`              // Jika Online (Vercel)
